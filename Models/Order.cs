7ff9e4a7c0467262299c29027fe4f41f5c3225cf
chanakya <chanakyal@zone24x7.com>
2023-07-12 11:35:32 +0530

    Add project files.

---
+++ Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Models
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public DateTime? OrderFulfilled { get; set; }
+        public Customer Customer { get; set; } = null!;
+        public ICollection<OrderDetail> OrderDetails { get; set; } = null!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using EntityFramework.Models;
+using EntityFramework.Data;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+
+Customer emily = new Customer()
+{
+    FirstName = "Emily",
+    LastName  = "Watson",
+    Address = "Adc Street",
+    Phone = "12345",
+    Email = "12345@mail.com",
+};
+context.Customers.Add(emily);
+
+Customer jake = new Customer()
+{
+    FirstName = "Jake",
+    LastName = "Johnson",
+    Address = "Bcd Street",
+    Phone = "1212",
+    Email = "1212@mail.com",
+};
+context.Customers.Add(jake);
+
+Customer peter = new Customer()
+{
+    FirstName = "Peter",
+    LastName = "Pan",
+    Address = "Cde Street",
+    Phone = "12345678",
+    Email = "12345678@mail.com",
+};
+context.Customers.Add(peter);
+context.SaveChanges();
